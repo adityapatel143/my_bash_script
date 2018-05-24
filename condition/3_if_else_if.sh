a586c7722e041001bac5dd566405fe272bc04e51
aditya patel <adityapatel081993@gmail.com>
2018-05-25 01:49:06 +0530

    odd and even validation using if_else

--- condition/3_if_else_if.sh
+++ condition/3_if_else_if.sh
@@ -5,29 +5,21 @@
 if command OR [condition]
 then
 	command to be executed 
-
 else
-
 	command failed so
-
 fi
 COMMENT
-
 #	OR
-
 <<COMMENT
 if test var -eq val
 then
         command to be executed
-
 else
-
         command failed so
-
 fi
 COMMENT
 
-# **** Program ****
+# **** Program sring ****
 
 read -p "enter the pass - hint is john : " pass
 
@@ -39,3 +31,13 @@ else
 	echo "Access denied"
 fi
 
+# **** Program no ****
+
+read -p "Entr the number : " num
+
+if test $num -ge 0
+then
+	echo "$num is a positive no."
+else
+	echo "$num ia a negtive no."
+fi
