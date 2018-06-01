9e30e19d2c9d1c1a566080211d37f57e334da594
aditya patel <aditya@mycompany.com>
2018-06-02 00:31:36 +0530

    added condition_checking_using_squire_bracket.sh

---
+++ condition/10_condition_checking_using_squire_bracket.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#The test command is used to check file types and compare values. 
+#You can also use [ as test command. It is used for:
+# • File attributes comparisons
+# • Perform string comparisons.
+# • Arithmetic comparisons.
+
+# Syntax
+#-----------------------------------------------------------------
+#	[ condition ]
+# 		OR
+#	[ ! condition ]
+#		OR
+#	[ condition ] && true-command
+#		OR
+#	[ condition ] || false-command
+#		OR
+#	[ condition ] && true-command || false-command 
+#=========================================================================
+
+[ 5 == 5 ] && echo "Yes" || echo "No"
+[ 5 == 15 ] && echo "Yes" || echo "No"
+[ 5 != 10 ] && echo "Yes" || echo "No"
+[ -f /etc/resolv.conf ] && echo "File /etc/resolv.conf found." || echo "File /etc/resolv.conf not found."
+[ -f /etc/resolv1.conf ] && echo "File /etc/resolv.conf found." || echo "File /etc/resolv.conf not found."
